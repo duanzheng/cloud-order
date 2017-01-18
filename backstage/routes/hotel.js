19f42727220ef1847d803621d1df8c35b5834ccd
TonyDuan <duanzheng21518@126.com>
2017-01-18 15:14:00 +0800

    新增酒店信息查询接口

--- backstage/routes/hotel.js
+++ backstage/routes/hotel.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models');
+var co = require('co');
 
 router.get('/insertHotel', function(req, res, next) {
     var ret = {};
@@ -14,7 +15,7 @@ router.get('/insertHotel', function(req, res, next) {
             var newHotelMsg = newHotel.get({
                 plain: true
             });
-            console.log(newHotelMsg);
+
             ret.error = 0;
             ret.msg = newHotelMsg;
             res.json(ret);
@@ -23,6 +24,39 @@ router.get('/insertHotel', function(req, res, next) {
         ret.error = 1;
         ret.msg = '请输入酒店信息';
         res.json(ret);
+    }
+});
+
+//查询酒店信息
+router.get('/getHotelInfo', function(req, res, next) {
+    var hotelId = req.query.id;
+    if (hotelId) {
+        co(function* () {
+            var hotel = yield models.hotel.findAll({
+                'where': {
+                    'id': hotelId
+                }
+            });
+
+            if (hotel.length > 0) {
+                res.json({
+                    error: 0,
+                    msg: hotel
+                });
+            } else {
+                res.json({
+                    error: 1,
+                    msg: '没有该酒店信息'
+                });
+            }
+        }).catch(function (e) {
+            console.log(e);
+        });
+    } else {
+        res.json({
+            error: 1,
+            msg: '缺少参数'
+        });
     }
 });
 
