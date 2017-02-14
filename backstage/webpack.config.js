870d7263ff249753f698ea292cabd27805566b08
Tony <duanzheng21518@126.com>
2017-02-14 22:37:11 +0800

    1

--- backstage/webpack.config.js
+++ backstage/webpack.config.js
@@ -2,39 +2,43 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
-  entry: [
-    'webpack-hot-middleware/client',
-    './views/app.js',
-  ],
-  output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
-  module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      include: [
-        path.resolve(__dirname, 'views'),
-      ],
-      loaders: ['react-hot-loader', 'babel-loader'],
-    }, {
-      test: /\.scss$/,
-      include: [
-        path.resolve(__dirname, 'views'),
-      ],
-      loader: 'style-loader!css-loader!sass-loader'
-    }],
-  },
-  resolve: {
-    alias: {
-      'react': path.join(__dirname, 'node_modules', 'react'),
+    devtool: 'cheap-module-eval-source-map',
+    entry: [
+        'webpack-hot-middleware/client',
+        'webpack/hot/dev-server',
+        './views/app.js',
+    ],
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+        publicPath: '/'
     },
-    extensions: ['.js', '.jsx', '.scss', '.css'],
-  },
-  plugins: [
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.HotModuleReplacementPlugin()
-  ],
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            include: [
+                path.resolve(__dirname, 'views'),
+            ],
+            loaders: ['react-hot-loader', 'babel-loader'],
+        }, {
+            test: /\.scss$/,
+            include: [
+                path.resolve(__dirname, 'views'),
+            ],
+            loader: 'style-loader!css-loader!sass-loader'
+        }],
+    },
+    resolve: {
+        alias: {
+            'react': path.join(__dirname, 'node_modules', 'react'),
+        },
+        extensions: ['.js', '.jsx', '.scss', '.css'],
+    },
+    plugins: [
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        inline: true
+    }
 };
